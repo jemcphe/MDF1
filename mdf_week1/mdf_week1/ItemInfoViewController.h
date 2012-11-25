989d0f5bc34424b4fa4e83a662f540cd24dbcc37
James McPherson <jemcphe@fullsail.edu>
2012-11-25 16:25:25 -0600

    Setting up  stock UITableView… I will create a separate ViewController for a custom UITableViewCell

--- mdf_week1/mdf_week1/ItemInfoViewController.h
+++ mdf_week1/mdf_week1/ItemInfoViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ItemInfoViewController.h
 //  mdf_week1
 //
 //  Created by James McPherson on 11/25/12.
@@ -8,9 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-{
-    //IBOutlets
-    IBOutlet UITableView* tableView;
-}
+@interface ItemInfoViewController : UIViewController
+
 @end
---
+++ mdf_week1/mdf_week1/ItemInfoViewController.m
@@ -0,0 +1,44 @@
+//
+//  ItemInfoViewController.m
+//  mdf_week1
+//
+//  Created by James McPherson on 11/25/12.
+//  Copyright (c) 2012 James McPherson. All rights reserved.
+//
+
+#import "ItemInfoViewController.h"
+
+@interface ItemInfoViewController ()
+
+@end
+
+@implementation ItemInfoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
